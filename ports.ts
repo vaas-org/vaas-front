ed250145506be27368cf471da7fe6c74f5f8efbc
Håkon Solbjørg <hakon@solbj.org>
2020-03-16 23:45:27 +0100

    feat: Convert ports JS to TS; add basic types and ports subscriptions

---
+++ ports.ts
@@ -0,0 +1,57 @@
+import { Elm } from "./src/Main.elm";
+
+const app = Elm.Main.init({
+    node: document.getElementById("app"),
+    flags: {},
+});
+
+if (app && app.ports && app.ports.sendvote)
+    app.ports.sendVote.subscribe(vote => console.log("sendvote: ", vote));
+else
+    console.log("error subbing")
+
+interface Alternative {
+    id: string;
+    title: string;
+}
+
+interface Vote {
+    id: string;
+    alternativeId: string;
+}
+
+interface Issue {
+    id: string;
+    title: string;
+    description: string;
+    state: "notstarted" | "inprogress" | "finished";
+    alternatives: Alternative[];
+    votes: Vote[];
+    maxVoters: number;
+};
+
+function sendIssueToElm(issue: Issue) {
+    app.ports.receiveIssue.send(issue);
+}
+
+setInterval(() => {
+    const i = {
+        id: "0fc3a2d",
+        title: "Some title",
+        description: "Fancy, long description with a lot of information but maybe not too much.",
+        // wtf typescript are you drunk
+        state: "notstarted" as "notstarted" | "inprogress" | "finished",
+        alternatives: [
+            { id: "1", title: "Alternative One" },
+            { id: "2", title: "Alternative Two" },
+        ],
+        votes: [
+            { id: "1", alternativeId: "1" },
+            { id: "2", alternativeId: "1" },
+            { id: "3", alternativeId: "2" },
+        ],
+        maxVoters: 10,
+    };
+    console.log("Sending issue");
+    sendIssueToElm(i);
+}, 3000)

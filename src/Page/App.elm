c8ac33daec603001c459265aa54d6fdef78ed8ae
Christian Duvholt <christian@duvholt.net>
2020-07-21 15:04:29 +0200

    Change to Browser.application for SPA routing and add very good landing page

--- src/Page/App.elm
+++ src/Page/App.elm
@@ -1,34 +1,54 @@
 module Page.App exposing (view)
 
-import Html exposing (Html, button, div, h1, header, span, text)
+import Browser
+import Html exposing (Attribute, Html, a, button, div, h1, header, span, text)
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
-import Model exposing (Client, ConnectionStatus(..), Model, Msg(..), Page(..))
+import Model exposing (Client, ConnectionStatus(..), Model, Msg(..))
 import Page.Admin
 import Page.Common exposing (connectionBullet)
 import Page.Error
 import Page.Loading
 import Page.Portal
 import Page.Vote exposing (issueContainer)
+import Route exposing (Route)
+
+
+type MenuItem
+    = Link { icon : String, route : Route, show : Bool }
+    | Button { icon : String, cmd : Msg, show : Bool }
 
 
 menuitems : Bool -> Maybe Client -> Html Msg
 menuitems isAdmin client =
     let
         items =
-            [ { icon = "🔧", cmd = RenderPage Config, show = True }
-            , { icon = "👨\u{200D}🚒", cmd = RenderPage Admin, show = isAdmin }
-            , { icon = "🚨", cmd = SendWebsocketConnect, show = client /= Nothing }
+            [ Link { icon = "🔧", route = Route.Config, show = True }
+            , Link { icon = "👨\u{200D}🚒", route = Route.Admin, show = isAdmin }
+            , Button { icon = "🚨", cmd = SendWebsocketConnect, show = client /= Nothing }
             ]
     in
     div [ style "display" "inline-block" ]
         (items
-            |> List.filter (\item -> item.show)
+            |> List.filter
+                (\item ->
+                    case item of
+                        Link l ->
+                            l.show
+
+                        Button b ->
+                            b.show
+                )
             |> List.map
                 (\item ->
-                    button
-                        [ style "display" "inline-block", onClick item.cmd ]
-                        [ text item.icon ]
+                    case item of
+                        Link l ->
+                            a
+                                [ style "display" "inline-block", Route.href l.route ]
+                                [ text l.icon ]
+
+                        Button b ->
+                            button [ style "display" "inline-block", onClick b.cmd ] [ text b.icon ]
                 )
         )
 
@@ -39,19 +59,28 @@ body model =
         [ style "grid-column" "2"
         , style "margin-top" "3rem"
         ]
-        [ case model.page of
-            App ->
+        [ case model.route of
+            Nothing ->
+                div [] [ text "Not found" ]
+
+            Just (Route.Meeting meetingId) ->
                 issueContainer model
 
-            Admin ->
+            Just Route.Admin ->
                 Page.Admin.view model
 
-            Config ->
+            Just Route.Config ->
                 div [] [ text "config" ]
+
+            Just Route.Landing ->
+                div []
+                    [ div [] [ text "Landing page yo" ]
+                    , a [ Route.href (Route.Meeting "1") ] [ text "United Nations meeting about corona" ]
+                    ]
         ]
 
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
     let
         isAdmin =
@@ -67,56 +96,60 @@ view model =
                 Nothing ->
                     False
     in
-    div
-        [ style "display" "grid"
-        , style "background-color" "#f5f5f5"
-        , style "height" "100%"
-        , style "min-height" "100vh"
-        , style "grid-template-rows" "4rem auto 4rem"
-        , style "grid-template-columns" "1fr min(80%, 1200px) 1fr"
+    { title = "VaaS"
+    , body =
+        [ div
+            [ style "display" "grid"
+            , style "background-color" "#f5f5f5"
+            , style "height" "100%"
+            , style "min-height" "100vh"
+            , style "grid-template-rows" "4rem auto 4rem"
+            , style "grid-template-columns" "1fr min(80%, 1200px) 1fr"
+            ]
+            [ banner isAdmin model.client
+            , case model.websocketConnection of
+                Connected ->
+                    case model.client of
+                        Just _ ->
+                            body model
+
+                        Nothing ->
+                            Page.Portal.view model.username
+
+                Reconnect _ ->
+                    Page.Loading.view
+
+                Connecting ->
+                    case model.client of
+                        Just _ ->
+                            Page.Loading.view
+
+                        Nothing ->
+                            Page.Portal.view model.username
+
+                NotConnectedYet ->
+                    case model.client of
+                        Just _ ->
+                            Page.Loading.view
+
+                        Nothing ->
+                            Page.Portal.view model.username
+
+                Disconnected ->
+                    Page.Error.view Nothing
+
+                Errored error ->
+                    Page.Error.view (Just error)
+
+                Disconnecting ->
+                    Page.Loading.view
+
+            -- we should technically show an error here
+            -- Page.Loading.view
+            , footer model.websocketConnection model.client
+            ]
         ]
-        [ banner isAdmin model.client
-        , case model.websocketConnection of
-            Connected ->
-                case model.client of
-                    Just _ ->
-                        body model
-
-                    Nothing ->
-                        Page.Portal.view model.username
-
-            Reconnect _ ->
-                Page.Loading.view
-
-            Connecting ->
-                case model.client of
-                    Just _ ->
-                        Page.Loading.view
-
-                    Nothing ->
-                        Page.Portal.view model.username
-
-            NotConnectedYet ->
-                case model.client of
-                    Just _ ->
-                        Page.Loading.view
-
-                    Nothing ->
-                        Page.Portal.view model.username
-
-            Disconnected ->
-                Page.Error.view Nothing
-
-            Errored error ->
-                Page.Error.view (Just error)
-
-            Disconnecting ->
-                Page.Loading.view
-
-        -- we should technically show an error here
-        -- Page.Loading.view
-        , footer model.websocketConnection model.client
-        ]
+    }
 
 
 banner : Bool -> Maybe Client -> Html Msg
@@ -138,7 +171,7 @@ banner isAdmin client =
                 [ style "margin-left" "1rem"
                 , style "color" "#f5f5f5"
                 ]
-                [ span [ onClick (RenderPage App) ] [ text "VaaS" ]
+                [ a [ Route.href Route.Landing ] [ text "VaaS" ]
                 , menuitems isAdmin client
                 ]
             ]
---
+++ src/Route.elm
@@ -0,0 +1,60 @@
+module Route exposing (Route(..), fromUrl, href, replaceUrl)
+
+import Browser.Navigation as Nav
+import Html exposing (Attribute)
+import Html.Attributes as Attr
+import Url exposing (Url)
+import Url.Parser as Parser exposing ((</>), Parser, oneOf, s, string)
+
+
+type Route
+    = Landing
+    | Meeting String
+    | Admin
+    | Config
+
+
+parser : Parser (Route -> a) a
+parser =
+    oneOf
+        [ Parser.map Landing Parser.top
+        , Parser.map Meeting (s "meeting" </> string)
+        , Parser.map Admin (s "admin")
+        , Parser.map Config (s "config")
+        ]
+
+
+href : Route -> Attribute msg
+href targetRoute =
+    Attr.href (routeToString targetRoute)
+
+
+fromUrl : Url -> Maybe Route
+fromUrl =
+    Parser.parse parser
+
+
+replaceUrl : Nav.Key -> Route -> Cmd msg
+replaceUrl key route =
+    Nav.replaceUrl key (routeToString route)
+
+
+routeToString : Route -> String
+routeToString route =
+    "/" ++ String.join "/" (routeToPieces route)
+
+
+routeToPieces : Route -> List String
+routeToPieces route =
+    case route of
+        Landing ->
+            []
+
+        Meeting id ->
+            [ "meeting", id ]
+
+        Admin ->
+            [ "admin" ]
+
+        Config ->
+            [ "config" ]
